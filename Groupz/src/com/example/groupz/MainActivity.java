326ab19ac594610a1c91d0986c179a7f043a2e57
gaurav1264 <gagaurav336@gmail.com>
2015-03-03 22:55:49 +0530

    Basic Mobile screen added

--- Groupz/src/com/example/groupz/MainActivity.java
+++ Groupz/src/com/example/groupz/MainActivity.java
@@ -1,22 +1,56 @@
 package com.example.groupz;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private EditText phoneNumbTv = null;
+	private CountDownTimer countDownTimer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		phoneNumbTv = (EditText) findViewById(R.id.ph_et);
+
+		findViewById(R.id.verifybutton).setOnClickListener(
+				new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						if (!TextUtils.isEmpty(phoneNumbTv.getText().toString())) {
+							verify();
+						}else{
+							Toast.makeText(getApplicationContext(), "Please enter your phone number to verify", 3500).show();
+						}
+
+					}
+				});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+	private void verify() {
+		countDownTimer = new CountDownTimer(60000, 1000) {
 
+			@Override
+			public void onTick(long millisUntilFinished) {
+			}
+
+			@Override
+			public void onFinish() {
+			}
+
+		};
+		countDownTimer.start();
+
+	}
+	private void showAlert(String message,boolean status){
+		
+	}
 }
